6f488df35389c9ef0e4dcfbecaf28dd86a0ced9a
jensonmaliakal <72599195+jensonmaliakal@users.noreply.github.com>
2025-02-14 11:37:21 +0530

    Update pgm1.java

--- pgm1.java
+++ pgm1.java
@@ -4,8 +4,8 @@ public class pgm1 {
         System.out.println("Hello, World!");
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the values of a and b");
-        int a=sc.nextInt();
-        int b=sc.nextInt();
+        String a=sc.next();
+        String b=sc.next();
         int sum=a+b;
         System.out.println("Sum="+sum);
     }
