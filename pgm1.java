2dbd6442767f3e84cf476aab17a85561ec810c3f
jensonmaliakal <72599195+jensonmaliakal@users.noreply.github.com>
2025-02-14 11:17:25 +0530

    Update pgm1.java

--- pgm1.java
+++ pgm1.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class pgm1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the values of a and b");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("Sum="+sum);
     }
 }
